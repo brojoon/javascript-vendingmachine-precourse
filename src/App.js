bd272af3754ff88212ea4d28d23a2f34c29819c8
BroJoon <ydngjink1234@gmail.com>
2022-01-23 00:49:18 +0900

    feat: dom 선택자 함수로 만듬, 초기 화면 띄워보기

---
+++ src/App.js
@@ -0,0 +1,7 @@
+export default class App {
+  constructor($target) {
+    $target.innerHTML = `<h1>자판기</div>
+		<div><button>상품 관리</button></div>
+		<section>섹션1</section>`;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from './App.js';
+import { $ } from './utils/dom-control.js';
+
+document.addEventListener('DOMContentLoaded', () => new App($('#app')));
+// document.querySelector('#app').innerText = '12313';
